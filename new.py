9769064a733f30a2a165811bce55f5649b89646c
UsedAIex <94004933+UsedAIex@users.noreply.github.com>
2022-02-08 19:51:30 +0300

    Готовимся к миссии

---
+++ new.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/<title>")
+def name_of_operation(title):
+   return render_template('first.html', title=title)
+
+
+# @app.route("/list_prof/<list_type>")
+# def profs(list_type):
+#
+#    return render_template('prof_list.html', title=title)
